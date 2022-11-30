50f6d3097ed81a29980d6ec3e69a3e98d3131521
balarajum2002 <mekalabalaraju777@gmail.com>
2022-11-30 12:06:57 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+/*import ClassComponent from "./ClassComponent.js"
+
+
+function App(){
+  return (
+    <div>
+    <p>Bala</p>
+    <h1>hello world</h1>
+    <ClassComponent/>
+    <img src="https://cdn.pixabay.com/photo/2021/06/29/06/14/water-drops-6373296__340.jpg"/>
+    </div>
+    )
+}
+export default App*/
+import PropsExample from './ClassComponent.js'
+function App(){
+  return(
+    <div>
+    <h1>I Am Bala</h1>
+    <PropsExample name="Bala" age="20"/>
+    </div>
+    )
+}
+export default App
